59ebf36792362de7e958e9ed7a70888424076f5a
X00136016 <X00136016@gmail.com>
2018-11-24 22:25:45 +0000

    Initial commit

---
+++ Python/Lab6/emailValidator.py
@@ -0,0 +1,19 @@
+import re
+
+#Read from the keyboard
+def readFromKB():
+    email = input("Enter an email address ->")
+    return email
+
+#Validate the email and print the result
+def validateEmail(email):
+    if re.match("[a-zA-Z0-9]+@gmail+\.(com|ie|co.uk)", email):
+        print( email, " is valid")
+    else:
+        print(email, " is not valid")
+
+def main():
+    email=readFromKB()
+    validateEmail(email)
+
+main()
